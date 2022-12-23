610850e6073f135a701b2b250bfe9b4bf3d83a23
Zionme <captainsinai@gmail.com>
2022-12-23 00:15:32 -0800

    3. I do not fear computers.

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * _puts - a function that prints a string
@@ -13,9 +14,9 @@ void _puts(char *str)
 
 	while (str[i] != '\0')
 	{
-		_putchar(str[i]);
+		putchar(str[i]);
 		i++;
 	}
-	_putchar('\n');
+	putchar('\n');
 }
 
