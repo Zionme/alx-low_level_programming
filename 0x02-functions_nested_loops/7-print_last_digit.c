945ef2c33e4997379fbe8e83573e87888c5490cb
Zionme <captainsinai@gmail.com>
2022-12-30 21:37:17 -0800

    7. There are only 3 colors, 10 digits, and 7 notes

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -15,7 +15,7 @@ int print_last_digit(int r)
 	else
 		n = r % 10;
 
-	putchar((n % 10) + '0');
+	_putchar((n % 10) + '0');
 	return (n % 10);
 }
 
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,1 +1,1 @@
-a function that prints the sign of a number
+a function that prints the last digit of a number
