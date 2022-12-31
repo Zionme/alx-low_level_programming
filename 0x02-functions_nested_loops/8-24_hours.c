dc3f5e378bb2a30cd842920e66a2d358289f3b3d
Zionme <captainsinai@gmail.com>
2022-12-30 21:43:05 -0800

    8. I am federal agent Jack Bauer, and today is the longest day of my life

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -15,7 +15,7 @@ void jack_bauer(void)
 		{
 			_putchar((H / 10) + '0');
 			_putchar((H % 10) + '0');
-			_putchar(":");
+			_putchar(':');
 			_Putchar((M / 10) + '0');
 			_putchar((M % 10) + '0');
 			_putchar('\n');
