5ed258791e750f1a7a63a7692c0b8d69d2e3810c
Zionme <captainsinai@gmail.com>
2023-01-01 20:27:39 -0800

    2. 0 > 972

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,7 +10,7 @@
 int largest_number(int a, int b, int c)
 {
 	int largest;
-	
+
 	if (a >= b && a >= c)
 	{
 		largest = a;
@@ -23,7 +23,7 @@ int largest_number(int a, int b, int c)
 	{
 		largest = c;
 	}
-	
+
 	return (largest);
 }
 
