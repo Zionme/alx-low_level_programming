503afbfdb98d38b13bd59426fe18c2dd2f404f1e
Zionme <captainsinai@gmail.com>
2023-01-29 00:36:06 -0800

    1. List length

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * list_len - a function that returns the number of elements in
+ * a linked list_t lust.
+ * @h: a pointer to list_t.
+ *
+ * Return: number of elements in h.
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t elements = 0;
+
+	while (h)
+	{
+		elements++;
+		h = h->next;
+	}
+
+	return (elements);
+}
+
