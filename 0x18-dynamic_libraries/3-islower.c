f25987aebfe3f7775dc90feca154b73f413ddd13
Zionme <captainsinai@gmail.com>
2023-03-20 11:44:54 -0700

    0. A library is not a luxury but one of the necessities of life

---
+++ 0x18-dynamic_libraries/3-islower.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _islower - check main
+ * @c: An input character
+ * Description: function uses _putchar function to print
+ * alphabet in lowercase 10 times, followed by a new line.
+ * Return: 1 if c is lowercase or 0 if c is uppercase.
+ */
+
+int _islower(int c)
+{
+	char i;
+	int lower = 0;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i == c)
+			lower = 1;
+	}
+
+	return (lower);
+}
+
---
+++ 0x18-dynamic_libraries/3-puts.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - a function that prints a string
+ * followed by a new line, to stdout.
+ * @str: An input string
+ *
+ * Return: Always 0.
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		putchar(str[i]);
+		i++;
+	}
+	putchar('\n');
+}
+
